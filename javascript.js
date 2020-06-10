c5f15252164969850e18f868c493e60b11250281
Pablo2369 <66697507+Pablo2369@users.noreply.github.com>
2020-06-09 20:48:04 -0500

    se agrega funcion

--- javascript.js
+++ javascript.js
@@ -11,6 +11,9 @@ $(function()
 		$("#Action3").click(function(){
 			alert("Clic en Something else here");
 		});
+		$("#Btnpablo").click(function(){
+			alert("bienvenido al perfil de pablo");
+		});
 	
 		 $( "#btnBienvenidoV" ).click(function() {
 		  alert( "Bienvenido a la opción de pagos" );
