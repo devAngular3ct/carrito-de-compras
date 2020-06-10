7191dd4576fe0c9d9de18e4d03cf0a13a92c4d2d
gvillamil <mem_020@hotmail.com>
2020-06-09 20:30:59 -0500

    Js para menu Memo
    Se agrega el JS para que funcionen los clic del menu Memo.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
+$(function()
+	{
+		$("#Action1").click(function(){
+			alert("Clic en Action");
+		});
 
+		$("#Action2").click(function(){
+			alert("Clic en Another action");
+		});
+
+		$("#Action3").click(function(){
+			alert("Clic en Something else here");
+		});
+	});
